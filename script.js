6641516bb9194fc1fd50b1ab6c26c7cdbd20864d
Matu <90573043+Ma1u0@users.noreply.github.com>
2025-12-13 00:11:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 var map = L.map('map').setView([54.5, 10], 5); // latitude, longitude, zoom
 
 // Add OpenStreetMap tiles
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
     subdomains: 'abcd',
     maxZoom: 19
 }).addTo(map);
