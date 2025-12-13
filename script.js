ed33c1922e418a26a2d203e6572d3a5b1ec2022c
Matu <90573043+Ma1u0@users.noreply.github.com>
2025-12-13 01:03:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 // Initialize the map centered over Europe
 var map = L.map('map').setView([54.5, 10], 5);
 
-// Default OpenStreetMap tiles
+// Base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    attribution: '&copy; OpenStreetMap contributors',
     maxZoom: 19
 }).addTo(map);
 
+/* =========================
+   ICON SETUP (ADD HERE)
+========================= */
+
+// Icon factory
+function createIcon(file) {
+    return L.icon({
+        iconUrl: `icons/${file}`,
+        iconSize: [36, 36],
+        iconAnchor: [18, 36],
+        popupAnchor: [0, -30]
+    });
+}
+
+// UAV icons
+const uavRed = createIcon("uav-red.png");
+const uavOrange = createIcon("uav-orange.png");
+const uavYellow = createIcon("uav-yellow.png");
+const uavBlue = createIcon("uav-blue.png");
+const uavGreen = createIcon("uav-green.png");
+
+// Jet icons
+const jetRed = createIcon("jet-red.png");
+const jetOrange = createIcon("jet-orange.png");
+const jetYellow = createIcon("jet-yellow.png");
+const jetBlue = createIcon("jet-blue.png");
+const jetGreen = createIcon("jet-green.png");
+
+// Balloon icons
+const balloonRed = createIcon("balloon-red.png");
+const balloonOrange = createIcon("balloon-orange.png");
+const balloonYellow = createIcon("balloon-yellow.png");
+const balloonBlue = createIcon("balloon-blue.png");
+const balloonGreen = createIcon("balloon-green.png");
+
+/* =========================
+   MARKERS (BELOW THIS)
+========================= */
+
 // Example marker
-L.marker([55.6761, 12.5683]) // Copenhagen coordinates
+L.marker([55.6761, 12.5683], { icon: uavRed })
     .addTo(map)
-    .bindPopup("<b>Copenhagen Airport</b><br>Date: 22 Sep 2025<br>Drone sighting");
+    .bindPopup("<b>Copenhagen Airport</b><br>Confirmed Russian UAV<br>22 Sep 2025");
