e8eb6055999dd4a3e620717fcd12f613666b0af2
Matu <90573043+Ma1u0@users.noreply.github.com>
2025-12-13 14:12:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-alert("SCRIPT LOADED");
+alert("JS IS RUNNING");
 
-// Create the map
 var map = L.map('map').setView([54.5, 10], 5);
 
-// Add OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors',
-  maxZoom: 19
+  attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Test marker
 L.marker([55.6761, 12.5683])
   .addTo(map)
-  .bindPopup("BASIC MARKER");
+  .bindPopup("TEST MARKER");
