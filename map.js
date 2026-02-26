2124b1ee548bb149c784e33e5cd16dce8674eb80
Matu <90573043+Ma1u0@users.noreply.github.com>
2026-02-26 19:43:53 +0100

    Update map.js

--- map.js
+++ map.js
@@ -2054,6 +2054,22 @@ document.addEventListener('DOMContentLoaded', () => {
     link: "https://www.reuters.com/world/traffic-halted-swedish-city-gothenburgs-airport-after-drone-sighting-daily-2025-11-06/",
     country: "Gothenburg Landvetter Airport, Sweden 🇸🇪"
   },
+
+  {
+    lat: 55.62063,
+    lng: 13.00378,
+    icon: icons.droneOrange,
+    type: 'drone',      // used for filtering
+    risk: 'orange',
+    place: 'militarybases',
+    year: '2026',
+    month: '02',
+    popupType: ' Drone sighting', // shown in popup
+    date: '26 Feb 2026, ',
+    details: "A drone flew to the French nuclear-powered aircraft carrier 'Charles de Gaulle'. According to Defence Minister Pål Jonson, the drone very likely started on a Russian vessel nearby. <br> A Swedish Navy vessel observed a drone and countermeasures were taken to disrupt the drone. It is unknown what happened to the drone once it disappeared. ",
+    link: "https://www.svt.se/nyheter/inrikes/rysk-dronare-narmade-sig-hangarfartyget-charles-de-gaulle",
+    country: "Malmö harbor, Sweden 🇸🇪"
+  },
     
      /* =========================
        T U R K E Y
